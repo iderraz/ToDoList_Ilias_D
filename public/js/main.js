6a662d95aaf53fbe24f15a05cb672bb9a1c3103d
iderraz <iderraz@hotmail.com>
2021-06-14 16:49:06 +0200

    ajout des btn avec click

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,100 @@ import {maDiv,masousDiv1,masousDiv2,masousDiv3,masousDiv4,masousDiv5,monh1,mabar
 document.body.style.backgroundColor = "teal";
 
 
-let thingsToDo;
+
+
+
+mabarre.addEventListener("keyup", (e) => {
+    if(e.keyCode == 13){
+        let thingsToDo = document.createElement("div");
+        thingsToDo.style.display = "flex";
+        thingsToDo.style.justifyContent = "space-around"
+        masousDiv4.style.justifyContent = "center"
+        masousDiv4.style.alignItems = "center"
+        thingsToDo.style.marginTop = "3%";
+    
+        let mabox = document.createElement("input");
+        mabox.setAttribute("type","checkbox");
+        thingsToDo.appendChild(mabox);
+        mabox.style.height = "50px";
+        mabox.style.width = "50px";
+    
+        let monp = document.createElement("p");
+        monp.innerText = mabarre.value;
+        thingsToDo.appendChild(monp);
+    
+        thingsToDo.style.border = "solid 5px grey"
+    
+    
+        let deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "delete";
+        let modifBtn = document.createElement("button");
+        modifBtn.innerText = "modifier";
+    
+        deleteBtn.addEventListener("click", () => {
+            
+            thingsToDo.style.display = "none"
+        });
+    
+    
+        thingsToDo.appendChild(deleteBtn);
+        thingsToDo.appendChild(modifBtn);
+        masousDiv4.appendChild(thingsToDo);
+        mabarre.value = "";
+
+
+        modifBtn.addEventListener("click", () =>{
+            monp.innerText = "";
+            let newinput = document.createElement("input");
+            thingsToDo.appendChild(newinput);
+            newinput.addEventListener("keyup", (e) =>{
+                          if(e.keyCode == 13){
+                             monp.innerText = newinput.value
+                             newinput.remove()
+                          }
+            })
+         });
+    
+        
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 monAdd.addEventListener("click", () =>{
     let thingsToDo = document.createElement("div");
     thingsToDo.style.display = "flex";
@@ -34,18 +127,50 @@ monAdd.addEventListener("click", () =>{
         thingsToDo.style.display = "none"
     });
 
-    // modifBtn.addEventListener("click", () => {
-    //     monp.textContent = "";
-    //     let inputor = document.createElement("input");
-    //     thingsToDo.appendChild(inputor)
-    //     monp.textContent = inputor.value;
-        
-    // })
 
     thingsToDo.appendChild(deleteBtn);
     thingsToDo.appendChild(modifBtn);
     masousDiv4.appendChild(thingsToDo);
     mabarre.value = "";
+
+    toDo.addEventListener("click", () => {
+        if (mabox.checked == false){
+            thingsToDo.style.display = "flex";
+            console.log("hello");
+          } else {
+            thingsToDo.style.display = "none";
+            console.log("hello");
+          }
+     });
+
+     done.addEventListener("click", () => {
+        if (mabox.checked == true){
+            thingsToDo.style.display = "flex";
+          } else {
+            thingsToDo.style.display = "none";
+          }
+     });
+
+     monall.addEventListener("click", () => {
+        if (mabox.checked == true || mabox.checked == false){
+            thingsToDo.style.display = "flex";
+        }
+     });
+
+     modifBtn.addEventListener("click", () =>{
+        monp.innerText = "";
+        let newinput = document.createElement("input");
+        thingsToDo.appendChild(newinput);
+        newinput.addEventListener("keyup", (e) =>{
+                      if(e.keyCode == 13){
+                         monp.innerText = newinput.value
+                         newinput.remove()
+                      }
+        })
+     });
+
+
+     
 }
  );
 
@@ -53,20 +178,13 @@ monclear.addEventListener("click",() =>{
     masousDiv4.innerHTML = "";
 })
 
-//  mabarre.addEventListener("keyup", (e) => {
-//      if(e.keycode == 13){
-//          let thingsToDo = document.createElement
-//      }
-//  })
 
 
-toDo.addEventListener("click", () => {
-   if (mabox.checked == true){
-       thingsToDo.style.display = "block";
-     } else {
-       thingsToDo.style.display = "none";
-     }
-})
+
+
+
+
+
 
 
 
